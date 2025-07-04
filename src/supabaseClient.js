63600fd1de6c32c88c4d2259cdb383e3b99f38a2
Wisley Tabosa <wtabosas@gmail.com>
2025-07-04 12:52:55 -0300

    update supa

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,12 +1,9 @@
 
+// Dentro do arquivo: src/supabaseClient.js
 
 import { createClient } from '@supabase/supabase-js'
 
-
-const supabaseUrl = 'https://tnhwvfryhdsjzfawqefg.supabase.co'
-
-
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRuaHd2ZnJ5aGRzanpmYXdxZWZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0ODQ3ODIsImV4cCI6MjA2NzA2MDc4Mn0.U-BQqdemz4HvrFWG0ybOJXVxb9CQQwvDKRGfUUVgaOU'
-
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
